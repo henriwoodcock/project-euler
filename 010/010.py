716c8bc93c7b7b11a8ce48cd60002996cb732445
henriwoodcock <henriwoodcock@gmail.com>
2020-06-21 16:09:45 +0100

    problem 10 python done

---
+++ 010/010.py
@@ -0,0 +1,20 @@
+'''
+010 Summation of primes
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+'''
+
+def primes_sieve(limit):
+    a = [True] * limit                          # Initialize the primality list
+    a[0] = a[1] = False
+
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            yield i
+            for n in range(i*i, limit, i):     # Mark factors non-prime
+                a[n] = False
+
+if __name__ == '__main__':
+    print(sum(primes_sieve(2000000)))
